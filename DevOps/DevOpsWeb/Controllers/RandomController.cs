4f4efcc8ae91cd48c96fedc5ebb8582ce591bf11
John Troch <JTCBE19@WP2288>
2016-10-25 10:42:20 +0200

    Randomcontroller added

---
+++ DevOps/DevOpsWeb/Controllers/RandomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevOpsWeb.Controllers
+{
+    public class RandomController : Controller
+    {
+        // GET: Random
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
